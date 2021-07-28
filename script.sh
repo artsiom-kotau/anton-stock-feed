384ef472d93f16d56766f4454b222914e788c43d
Palshyn <a.palshyn@gmail.com>
2021-07-28 17:45:58 +0300

    updated script

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
+tomcat_path=$1
 find src/ -name "*.java" > classes
 mkdir -p anton-stock-feed-servlet/WEB-INF/classes
 mkdir -p anton-stock-feed-servlet/WEB-INF/lib
-javac -d anton-stock-feed-servlet/WEB-INF/classes -cp "C:/Users/Anton/Documents/Java projects/Stock Feed/anton-stock-feed/additional libraries/servlet-api.jar;C:/Users/Anton/Documents/Java projects/Stock Feed/anton-stock-feed/additional libraries/gson-2.8.7.jar;" -sourcepath  "src/" @classes
+javac -d anton-stock-feed-servlet/WEB-INF/classes -cp "additional libraries/servlet-api.jar;additional libraries/gson-2.8.7.jar;" -sourcepath  "src/" @classes
 cp "additional libraries/gson-2.8.7.jar" "anton-stock-feed-servlet/WEB-INF/lib"
 cp "additional libraries/servlet-api.jar" "anton-stock-feed-servlet/WEB-INF/lib"
 cp "additional libraries/postgresql-42.2.22.jar" "anton-stock-feed-servlet/WEB-INF/lib"
@@ -12,4 +13,16 @@ cd anton-stock-feed-servlet/
 jar -cvf anton-stock-feed-servlet.war *
 war_file_name=$(find . -name "*.war")
 echo $war_file_name
-cp $war_file_name "C:\Program Files\apache-tomcat-9.0.48-windows-x64\apache-tomcat-9.0.48\webapps"
+tomcat_webapps_path="$tomcat_path/webapps"
+cp $war_file_name "$tomcat_webapps_path"
+catalina_properties_path="$tomcat_path/conf"
+echo $catalina_properties_path
+cd "$catalina_properties_path"
+echo "">>catalina.properties
+echo "DAO=Database">>catalina.properties
+echo "">>catalina.properties
+echo "ProfileService=Database">>catalina.properties
+echo "">>catalina.properties
+echo "JsonSerialize=Gson">>catalina.properties
+echo "">>catalina.properties
+echo "APIService=StandardHTTP">>catalina.properties
